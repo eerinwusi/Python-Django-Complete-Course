d1afa4ee39fbfcb65f25e1fe2576994e8ee1ab96
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-08-31 09:03:42 +0100

    Models & Admin Panel, Database Relationships

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,48 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+from django.db.models import CASCADE
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    # the tags field below establishes a many-to-many relationship between the Projects class and the Tags class.
+    # the tag is in single quote because it is defined after the Projects class.
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Reviews(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    # owner =
+    # the project field below establishes a one-to-many relationship between the Projects class and Reviews class
+    project = models.ForeignKey(Projects, on_delete=CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
